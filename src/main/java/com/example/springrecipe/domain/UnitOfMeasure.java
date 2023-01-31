6a58bd43bb188c03010bdd27a61b4e17edddeeb1
Joseph Jacob <josephjacob1998@gmail.com>
2023-01-31 08:25:59 +0530

    complete assignment : add unit of measurement

---
+++ src/main/java/com/example/springrecipe/domain/UnitOfMeasure.java
@@ -0,0 +1,31 @@
+package com.example.springrecipe.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class UnitOfMeasure {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private  String uom;
+
+    public String getUom() {
+        return uom;
+    }
+
+    public void setUom(String uom) {
+        this.uom = uom;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+}
