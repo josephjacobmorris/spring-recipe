a2655a1abea3004e519ce7a0b4f4886eef9f6e3e
Joseph Jacob <josephjacob1998@gmail.com>
2023-02-09 08:25:51 +0530

    adding testcase for index controller

---
+++ src/test/java/com/example/springrecipe/controllers/IndexControllerTest.java
@@ -0,0 +1,76 @@
+package com.example.springrecipe.controllers;
+
+import com.example.springrecipe.service.RecipeService;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.ui.Model;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(SpringExtension.class)
+
+public class IndexControllerTest {
+    @Mock
+    RecipeService recipeService;
+
+    @InjectMocks
+    IndexController indexController;
+    @Test
+    public void testIndex() {
+        Model model = new Model() {
+            @Override
+            public Model addAttribute(String attributeName, Object attributeValue) {
+                return null;
+            }
+
+            @Override
+            public Model addAttribute(Object attributeValue) {
+                return null;
+            }
+
+            @Override
+            public Model addAllAttributes(Collection<?> attributeValues) {
+                return null;
+            }
+
+            @Override
+            public Model addAllAttributes(Map<String, ?> attributes) {
+                return null;
+            }
+
+            @Override
+            public Model mergeAttributes(Map<String, ?> attributes) {
+                return null;
+            }
+
+            @Override
+            public boolean containsAttribute(String attributeName) {
+                return false;
+            }
+
+            @Override
+            public Object getAttribute(String attributeName) {
+                return null;
+            }
+
+            @Override
+            public Map<String, Object> asMap() {
+                return null;
+            }
+        };
+        when(recipeService.findAll()).thenReturn(Collections.emptySet());
+        String actualOp = indexController.index(model);
+        assertEquals("index", actualOp);
+    }
+}
