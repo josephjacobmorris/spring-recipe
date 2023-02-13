0c5dde0accd0d80b1499e25dc07d4227265ece1a
Joseph Jacob <josephjacob1998@gmail.com>
2023-02-13 09:05:52 +0530

    adding controller advice for Not found  exception

---
+++ src/main/java/com/example/springrecipe/controllers/ControllerExceptionHandler.java
@@ -0,0 +1,20 @@
+package com.example.springrecipe.controllers;
+
+import com.example.springrecipe.exceptions.NotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.servlet.ModelAndView;
+
+@ControllerAdvice
+public class ControllerExceptionHandler {
+    @ExceptionHandler(NotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ModelAndView handleNotFoundException(Exception exception){
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("404error");
+        modelAndView.addObject("exception", exception);
+        return modelAndView;
+    }
+}
